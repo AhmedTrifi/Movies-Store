b0dce275631c15919480a4b4a211e049b62208fc
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 18:11:03 +0100

    fixing get methods by refactoring all the controllers methods with async await

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,45 +1,47 @@
 const Movie = require('../models/movie')
 
 module.exports ={
-    addMovie :  (req, res)=>{
+    addMovie :  async (req, res)=>{
+        console.log(req.body);
         try {
-            Movie.create(req.body)
+            await Movie.create(req.body)
             res.status(201).json({message:"your movie review added successfuly" })
         } catch (error) {
             res.status(400).json({message:"error creating movie review"})
             console.log(error)
         }
     },
-    getMovies :  (req, res)=>{
+    getMovies : async (req, res)=>{
         try {
-          const data = Movie.find()
+          const data = await Movie.find()
+          console.log(data);
           res.status(201).json(data)
         } catch (error) {
             res.status(400).json({message:"something went wrong"})
             console.log(error)
         }
     },
-    getOneMovie :  (req, res)=>{
+    getOneMovie : async (req, res)=>{
         try {
-            const data = Movie.findOne({_id:req.params.id})
+            const data = await Movie.findOne({_id:req.params.id})
             res.status(201).json(data)
           } catch (error) {
               res.status(400).json({message:"something went wrong"})
               console.log(error)
           }
     },
-    deleteMovie :  (req, res)=>{
+    deleteMovie :  async (req, res)=>{
         try {
-            Movie.deleteOne({_id:req.params.id})
+           await Movie.deleteOne({_id:req.params.id})
             res.status(201).json({message:"Movie deleted successfully"})
           } catch (error) {
               res.status(400).json({message:"something went wrong"})
               console.log(error)
           }
     },
-    updateMovie :  (req, res)=>{
+    updateMovie : async (req, res)=>{
         try {
-            Movie.findByIdAndUpdate({_id:req.params.id},req.body)
+           await Movie.findByIdAndUpdate({_id:req.params.id},req.body)
            res.status(201).json({message:"Movie updated successfully"})
          } catch (error) {
              res.status(400).json({message:"something went wrong"})
--- server/routes/movies.js
+++ server/routes/movies.js
@@ -7,8 +7,8 @@ const MoviesController = require('../controllers/movies');
 
 router.post("/addMovie",MoviesController.addMovie)
 router.get("/getMovies",MoviesController.getMovies)
-router.get("/getOneMovie",MoviesController.getOneMovie)
-router.delete("/deleteMovie",MoviesController.deleteMovie)
-router.put("/updateMovie",MoviesController.updateMovie)
+router.get("/getOneMovie/:id",MoviesController.getOneMovie)
+router.delete("/deleteMovie/:id",MoviesController.deleteMovie)
+router.put("/updateMovie/:id",MoviesController.updateMovie)
 
 module.exports = router
