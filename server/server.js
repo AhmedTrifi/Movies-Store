78deff29fc7ca9b8f8312164e8673d8002bbd5ab
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 17:41:08 +0100

    called the movie route in the express server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require ('./config/db')
+const movieRoute = require('./routes/movies')
 
 require("dotenv").config();
 const app = express();
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3070
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/public"));
+app.use('/',movieRoute)
+
 
 app.listen(PORT, function () {
     console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
