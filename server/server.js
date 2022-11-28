ec46b1c1a039ebf3a8aca71b2d08a6b73c450237
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 21:03:02 +0100

    back works

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require ('./config/db')
 const movieRoute = require('./routes/movies')
-
+const cors = require('cors')
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3070
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../client/public"));
 app.use('/movie',movieRoute)
 
+app.use(cors())
 
 app.listen(PORT, function () {
     console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
