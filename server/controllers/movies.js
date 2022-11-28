410b50df6d35598126b18b528f654cc0deb6184b
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 16:47:51 +0100

    added the movie's controllers methodes skeleton

---
+++ server/controllers/movies.js
@@ -0,0 +1,19 @@
+const Movie = require('../models/movie')
+
+module.exports ={
+    addMovie :  (req, res)=>{
+
+    },
+    getMovies :  (req, res)=>{
+
+    },
+    getOneMovie :  (req, res)=>{
+
+    },
+    deleteMovie :  (req, res)=>{
+
+    },
+    updateMovie :  (req, res)=>{
+
+    },
+}
