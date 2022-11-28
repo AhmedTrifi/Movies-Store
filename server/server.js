54f1ba020de3450d938292e1e92c82e1d7d323e0
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 16:32:04 +0100

    express server added to the backend

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3070
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/public"));
+
+app.listen(PORT, function () {
+    console.log(`Server Runs Perfectly at http://localhost:${PORT}`);
+  });
