17e05395b29aa66c7935290aa54c352ecc09b202
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 19:23:30 +0100

    adding the navbar in the header component and the route in the app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-
+import AddMovie from './components/AddMovie';
+import AllMovies from './components/AllMovies';
+import Header from './components/Header';
+import OneMovie from './components/OneMovie';
+import {BrowserRouter,Route,Routes} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <div className="bg-white" style={{ height: "100vh" }}>
+        <Header  />
+        <Routes>
+          <Route
+            path="/"
+            element={
+                 <AllMovies />
+             }
+          />
+          <Route
+            path="/add"
+            element={
+                 <AddMovie />
+             }
+          />
+          <Route
+            path="/:id"
+            element={
+                <OneMovie />
+            }
+          />
+        </Routes>
+       </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Header() {
   return (
-    <div>Header</div>
-  )
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Taste</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav mr-auto">
+  <li class="nav-item active">
+
+          <Link class="nav-link " to="/">Home</Link>
+          </li>
+          <li class="nav-item active">
+
+         <Link class="nav-link " to="/add">Add Review</Link>
+         </li>
+       </ul>
+   </div>
+</nav>  )
 }
 
 export default Header
