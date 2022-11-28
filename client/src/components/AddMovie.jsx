d565486c8ad1533ce78b43eb21700e30139af70d
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-28 19:48:33 +0100

    adding the onchageHundler and submithundler methods to the addMovie component

--- client/src/components/AddMovie.jsx
+++ client/src/components/AddMovie.jsx
@@ -1,36 +1,54 @@
 import React from 'react'
-
+import { useState } from 'react'
+import axios from 'axios'
+import useNavigate from 'react-router-dom'
 function AddMovie() {
-  return (
-    <div className="container">
-        <form>
-  <div class="form-group">
-    <label for="name">name</label>
-    <input type="text" class="form-control" id="name"  placeholder="Enter movie Name" />
-  </div>
-  <div class="form-group">
-    <label for="gender">Movie Gender</label>
-    <select class="form-control" id="gender">
-      <option>Horror</option>
-      <option>Romantic</option>
-      <option>Fantasy</option>
-      <option>Drama</option>
-      <option>Action</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="image">Image</label>
-    <input type="text" class="form-control" id="image" placeholder="Enter your movie image url"/>
-  </div>
-  <div class="form-group">
-    <label for="description">Movie Review</label>
-    <textarea class="form-control" id="description" rows="3"></textarea>
-  </div>
+    [form, setForm] = useState({})
+    const navigate =useNavigate()
+    const onchangeHandler=(e)=>{
+        setForm({
+            ...form ,
+            [e.target.name]: e.target.value
+        })
+    }
+    const submitHundler = (e)=>{
+        e.preventDefault()
+        axios.post("/movie/addMovie",form)
+        .then(response=>{
+            alert(response.data.message)
+            navigate("/")
+        })
+    }
+    return (
+        <div className="container">
+            <form onSubmit={submitHundler}>
+                <div class="form-group">
+                    <label for="name">name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter movie Name" onChange={onchangeHandler}/>
+                </div>
+                <div class="form-group">
+                    <label for="gender">Movie Gender</label>
+                    <select class="form-control" name="gender" onChange={onchangeHandler}>
+                        <option>Horror</option>
+                        <option>Romantic</option>
+                        <option>Fantasy</option>
+                        <option>Drama</option>
+                        <option>Action</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="text" class="form-control" name="image" placeholder="Enter your movie image url" onChange={onchangeHandler}/>
+                </div>
+                <div class="form-group">
+                    <label for="description">Movie Review</label>
+                    <textarea class="form-control" name="description" rows="3" onChange={onchangeHandler}></textarea>
+                </div>
 
 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+                <button type="submit" class="btn btn-primary" >Submit</button>
+            </form>
+        </div>
     )
 }
 
