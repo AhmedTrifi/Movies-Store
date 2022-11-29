267608d09fc18ea53a7195343c14b8cddd8548a2
unknown <ahmed.trifi007@gmail.comgit config --global user.name AhmedTrifi>
2022-11-29 09:32:08 +0100

    fixed css style and some error

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,25 @@
-import './App.css';
-import AddMovie from './components/AddMovie';
-import AllMovies from './components/AllMovies';
-import Header from './components/Header';
-import OneMovie from './components/OneMovie';
-import {BrowserRouter,Route,Routes} from "react-router-dom"
+import "./App.css";
+import AddMovie from "./components/AddMovie";
+import AllMovies from "./components/AllMovies";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UpdateMovie from "./components/UpdateMovie";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="bg-white" style={{ height: "100vh" }}>
-        <Header  />
         <Routes>
-          <Route
-            path="/"
-            element={
-                 <AllMovies />
-             }
-          />
-          <Route
-            path="/add"
-            element={
-                 <AddMovie />
-             }
-          />
-          {/* <Route
-            path="/:id"
-            element={
-                <OneMovie />
-            }
-          /> */}
+        <Route index element={<Signin />} />
+
+          <Route path="/home" element={<AllMovies />} />
+          <Route path="/add" element={<AddMovie />} />
+          <Route path="/:id" element={<UpdateMovie />} />
+          <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         </Routes>
-       </div>
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 function Header() {
   return (
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Taste</a>
+  <Link class="navbar-brand" to="#">Taste</Link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -11,12 +11,13 @@ function Header() {
   <ul class="navbar-nav mr-auto">
   <li class="nav-item active">
 
-          <Link class="nav-link " to="/">Home</Link>
+          <Link class="nav-link " to="/home">Home</Link>
           </li>
           <li class="nav-item active">
 
          <Link class="nav-link " to="/add">Add Review</Link>
          </li>
+    
        </ul>
    </div>
 </nav>  )
